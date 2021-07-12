0c7d3f9672cc8f2fbb9b4207cffe027649f2a674
김은영 <solaz0824@gmail.com>
2021-07-12 18:50:54 +0200

    queries changed and works well, readme done

--- README.md
+++ README.md
@@ -1,15 +1,56 @@
+
+
 # 💊 MySQL Basics
-
 _MySQL, SQL language, setting environment variables._
-
+</br></br>
 ## 📔 Description
 
 Intro pill which served as our first contact with the DBMS "MySQL", where we set an environment variable to be able to run the program from the command line, imported an example database from a ".sql" file provided by the official MySQL team, and executed some queries to play around with a DB and to getting familiar with the SQL language.
-
+</br></br>
 ### 📂 Content
 
 This repository contains only the following files:
 
 - **_README.md_**: this explanatory file.
 - **_employees.sql_**: file containing an example database provided by the official MySQL team, to import and use in our exercise.
-- **_queries.sql_**: file where we recorded all the necessary queries executed to fulfill this exercise.
+- **_queries.sql_**: file where we recorded all the necessary queries executed to fulfill this exercise.  
+  </br></br>
+
+
+## 🏁 Getting Started 
+</br>
+1. clone the repository</br>
+   </br>
+
+```
+git clone https://github.com/solaz0824/mysql-basics.git
+```
+</br>
+2. install MAMP or XAMPP
+   
+- For this project you need a version of MySQL database server equal to or greater than 8.0.
+
+- If you have already installed XAMPP or MAMP, you can only download the MySQL visual interface called Workbench.
+
+- download here: https://dev.mysql.com/downloads/workbench/
+
+</br>
+3. Import an example database in  
+   </br>
+
+```
+employees.sql 
+```
+</br>
+4. Run queries in query.sql file respectively.     
+   </br></br></br>
+   
+## ✍️ Author
+ [@solaz0824](https://github.com/solaz0824) 
+
+
+
+
+
+
+
--- queries.sql
+++ queries.sql
@@ -1,266 +1,302 @@
--- MySQL Basics:
 
--- 1. DATA INSERTION:
+/* INSERT DATA */
 
-  -- Table "employees":
-  -- First test insertion for one single register
-  INSERT INTO 
-    employees (emp_no, birth_date, first_name, last_name, gender, hire_date) 
-    values (1, '1986-12-23', 'Jorge', 'Garcia', 'M', '2020-01-01');
-  -- Multiple insertion for the remaining elements
-  INSERT INTO 
-    employees (emp_no, birth_date, first_name, last_name, gender, hire_date) 
-    values 
-      (2, '2000-06-15', 'John', 'Smith', 'M', '2020-08-01'), 
-      (3, '1997-03-19', 'Jake', 'Anderson', 'M', '2018-12-12'), 
-      (4, '1960-01-06', 'Jacob', 'Ford', 'M', '1995-03-27'), 
-      (5, '1955-11-30', 'Emilia', 'López', 'F', '1995-03-27'), 
-      (6, '1984-02-28', 'Anne', 'Brooks', 'F', '2004-07-01'), 
-      (7, '1970-04-22', 'Mary', 'Nichols', 'F', '2000-02-01'), 
-      (8, '1979-10-09', 'Anne', 'Biden', 'F', '2010-09-01'), 
-      (9, '1969-07-31', 'Anthony', 'Keyssar', 'M', '2001-11-11'), 
-      (10, '1990-08-12', 'Diana', 'Browning', 'F', '2010-05-16'), 
-      (11, '1989-09-11', 'Vincent', 'Waller', 'M', '2014-10-20'), 
-      (12, '1981-05-15', 'Anne', 'Wu', 'F', '2008-04-22'), 
-      (13, '1977-12-08', 'Albert', 'Kitroeff', 'M', '1999-12-31'), 
-      (14, '1995-07-03', 'Jennifer', 'Sánchez', 'F', '2019-06-03'), 
-      (15, '1982-09-23', 'Charles', 'Glueck', 'M', '2007-09-29');
+/* Insert 15 employees */ 
+INSERT INTO 
+employees (emp_no, birth_date, first_name, last_name, gender, hire_date)
+values
+(1, '1986-08-24', 'EY', 'Kim', 'F', '2019-11-10'),
+(2, '1987-11-24', 'Brandon', 'Parry', 'M', '2020-01-10'),
+(3, '1982-12-01', 'William', 'Thomas', 'M', '2018-05-01'),
+(4, '1989-01-17', 'Lois', 'Hansen', 'F', '2018-06-30'),
+(5, '1984-03-02', 'John', 'Copeland', 'M', '2016-10-11'),
+(6, '1986-04-12', 'Henry', 'Himes', 'M', '2014-11-11'),
+(7, '1981-12-28', 'Betty', 'Roberts', 'F', '2017-09-23'),
+(8, '1990-11-03', 'Cathy', 'Willcox', 'F', '2020-11-07'),
+(9, '1988-07-21', 'Alan', 'Young', 'M', '2011-10-22'),
+(10, '1989-01-22', 'John', 'Marks', 'M', '2018-01-19'),
+(11, '1981-12-12', 'Melissa', 'Stone', 'F', '2021-01-03'),
+(12, '1983-08-02', 'John', 'Rolland', 'M', '2010-09-30'),
+(13, '1984-03-27', 'David', 'Morgan', 'M', '2011-10-01'),
+(14, '1986-01-31', 'Jenifer', 'Dizon', 'F', '2012-11-29'),
+(15, '1991-12-25', 'Carole', 'Parker', 'F', '2015-06-18');
 
-  -- Table "salaries":
-  INSERT INTO 
-    salaries (emp_no, salary, from_date, to_date) 
-    values 
-      (1, 25000, '2020-01-01', now()), 
-      (2, 42300, '2020-08-01', now()), 
-      (3, 19800.70, '2018-12-12', '2020-03-31'), 
-      (3, 33459.23, '2020-04-01', now()), 
-      (4, 28000, '1995-03-27', now()), 
-      (5, 9950, '1995-03-27', now()), 
-      (6, 8000, '2004-07-01', now()), 
-      (7, 18000, '2000-02-01', '2009-12-31'), 
-      (7, 28750.55, '2010-01-01', now()), 
-      (8, 35000, '2010-09-01', '2018-06-30'), 
-      (8, 39000, '2018-07-01', now()), 
-      (9, 44400, '2001-11-11', now()), 
-      (10, 21900, '2010-05-16', now()), 
-      (11, 38000, '2014-10-20', '2019-10-01'), 
-      (11, 49575, '2019-10-02', now()), 
-      (12, 15300, '2008-04-22', now()), 
-      (13, 6900, '1999-12-31', now()), 
-      (14, 12100, '2019-06-03', now()), 
-      (15, 32000, '2007-09-29', '2012-04-22'), 
-      (15, 40000, '2012-04-23', now());
 
-  -- Table "departments":
-  INSERT INTO 
-    departments (dept_no, dept_name) 
-    values 
-      ('01a', 'Finance'), 
-      ('01b', 'Administration'), 
-      ('02a', 'Sales'), 
-      ('02b', 'Procurement'), 
-      ('03', 'Logistics');
 
-  -- Table "dept_manager":
-  INSERT INTO 
-    dept_manager (emp_no, dept_no, from_date, to_date) 
-    values 
-      (1, '01a', '2020-03-01', now()), 
-      (5, '01b', '2000-09-30', '2020-01-01'), 
-      (6, '01b', '2020-01-02', now()), 
-      (7, '02a', '2010-12-01', '2020-01-01'), 
-      (5, '02a', '2020-01-02', now()), 
-      (11, '02b', '2016-11-15', now()), 
-      (12, '03', '2012-05-25', now());
+/* Insert salaries */
 
-  -- Table "dept_emp":
-  INSERT INTO 
-    dept_emp (emp_no, dept_no, from_date, to_date) 
-    values 
-    (1, '01b', '2020-01-01', '2020-02-29'), 
-    (1, '01a', '2020-03-01', now()), 
-    (2, '03', '2020-08-05', now()), 
-    (3, '02b', '2018-12-12', now()), 
-    (4, '02a', '1995-03-27', '2010-03-31'), 
-    (4, '02b', '2010-01-04', now()), 
-    (5, '01b', '1995-03-27', '2020-01-01'), 
-    (5, '01a', '2020-01-02', now()), 
-    (6, '01b', '2004-07-01', now()), 
-    (7, '02a', '2000-02-01', now()), 
-    (8, '03', '2010-09-01', '2015-09-10'), 
-    (8, '02a', '2015-09-11', now()), 
-    (9, '02b', '2001-11-11', '2012-12-11'), 
-    (9, '03', '2012-12-12', now()), 
-    (10, '03', '2010-05-16', now()), 
-    (11, '02b', '2014-10-20', '2019-07-15'), 
-    (11, '03', '2019-07-16', now()), 
-    (12, '03', '2008-04-22', '2019-07-15'), 
-    (12, '02b', '2019-07-16', now()), 
-    (13, '01b', '1999-12-31', '2018-12-31'), 
-    (13, '02a', '2019-01-01', now()), 
-    (14, '02b', '2019-06-03', '2020-06-29'), 
-    (14, '03', '2020-06-30', now()), 
-    (15, '01a', '2007-09-29', '2017-08-30'), 
-    (15, '03', '2017-08-31', now());
+INSERT INTO
+salaries (emp_no, salary, from_date, to_date) 
+values 
+(1, 50000, '2019-11-10', now()),
+(2, 16000, '2020-01-10', now()),
+(3, 23000, '2018-05-01', '2020-12-31'),
+(3, 43000, '2021-01-01', now()),
+(4, 20000, '2018-06-30', '2020-08-31'),
+(4, 38000, '2020-09-01', now()),
+(5, 43000, '2016-10-11', now()),
+(6, 19000, '2014-11-11', '2016-12-31'),
+(6, 30000, '2017-01-01', '2018-12-31'),
+(6, 45000, '2019-01-01', now()),
+(7, 42500, '2017-09-23', '2019-12-31'),
+(7, 50000, '2020-01-01', now()),
+(8, 39000, '2020-11-07', now()),
+(9, 23000, '2011-10-22', '2013-12-31'),
+(9, 37600, '2014-01-01', '2015-12-31'),
+(9, 42300, '2016-01-01', '2018-12-31'),
+(9, 49900, '2019-01-01', now()),
+(10, 38100, '2018-01-19', now()),
+(11, 23520, '2021-01-03', now()),
+(12, 47700, '2010-09-30', now()),
+(13, 34420, '2011-10-01', now()),
+(14, 41260, '2012-11-29', now()),
+(15, 29990, '2015-06-18', now());
 
-  -- Table "titles":
-  INSERT INTO 
-    titles (emp_no, title, from_date, to_date) 
-    values 
-      (1, 'Degree in Civil Engineering', '2011-09-25', null), 
-      (2, 'BA in Business Administration', '2020-06-13', null), 
-      (3, 'BA in Law', '2016-10-28', null), 
-      (4, 'BA in Economics', '1985-12-03', null), 
-      (5, 'Degree in Mechanical Engineering', '2020-02-02', null), 
-      (6, 'BA in Architecture', '2006-08-30', null), 
-      (7, 'BA in Psychology', '1994-07-07', null), 
-      (8, 'Degree in Computer Science', '2020-03-31', null), 
-      (9, 'Degree in Applied Mathematics', '1992-01-01', null), 
-      (10, 'Degree in Physics', '2014-06-27', null), 
-      (11, 'BA in English Philology', '2020-05-17', null), 
-      (12, 'BA in Law', '2003-12-19', null), 
-      (13, 'BA in Business Administration', '2000-09-24', null), 
-      (14, 'BA in Psychology', '2020-01-10', null), 
-      (15, 'BA in Economics', '2005-04-23', null);
 
--- 2. DATA UPDATE:
 
-  -- Update employees: change the name of an employee. To do this, generate a query that affects only a certain employee based on their name, surname and date of birth.
-  UPDATE employees 
-    SET first_name = 'Carmen'
-    WHERE (
-      first_name = 'Anne' AND 
-      last_name LIKE '%b%' AND 
-      birth_date > '1980-01-01'
-    );
+/* Insert departments */
 
-  -- Update departments: change the name of all departments.
-  UPDATE departments 
-    SET dept_name = 
-      CASE 
-        WHEN dept_no = '01a' THEN 'Finance & Administration' 
-        WHEN dept_no = '01b' THEN 'HR, Expats & Payroll' 
-        WHEN dept_no = '02a' THEN 'Sales & Customer Service' 
-        WHEN dept_no = '02b' THEN 'Procurement & Supplier Relationship' 
-        WHEN dept_no = '03' THEN 'Logistics & SCM' 
-      END;
+INSERT INTO 
+departments (dept_no, dept_name)
+values
+('001', 'Human Resource Management'),
+('002', 'Production'),
+('003', 'Purchasing'),
+('004', 'Marketing'),
+('005', 'Account and Finance');
 
--- 3. DATA OBTAINMENT:
 
-  -- Select all employees with a salary greater than 20,000.
+
+/* Insert departments managers */
+
+INSERT INTO
+dept_manager (emp_no, dept_no, from_date, to_date)
+values 
+(1, '001', '2019-11-10', now()),
+(5, '002', '2016-10-11', now()),
+(6, '003', '2019-01-01', now()),
+(9, '004', '2016-01-01', '2018-12-31'),
+(10, '004', '2019-01-01', now()),
+(12, '005', '2010-09-30', now());
+
+
+
+/* Insert employess in each department */
+
+INSERT INTO
+dept_emp (emp_no, dept_no, from_date, to_date)
+values
+(1, '001', '2019-11-10', now()),
+(2, '002', '2020-1-10', '2020-12-31'),
+(2, '003', '2021-01-01', now()),
+(3, '003', '2018-05-01', '2020-12-31'),
+(3, '002', '2021-01-01', now()),
+(4, '005', '2018-06-30', '2020-08-31'),
+(4, '004', '2020-09-01', now()),
+(5, '002', '2016-10-11', now()),
+(6, '003', '2014-11-11', now()),
+(7, '005', '2017-09-23', '2019-12-31'),
+(7, '001', '2020-01-01', now()),
+(8, '001', '2020-11-07', '2021-03-01'),
+(8, '004', '2021-03-02', now()),
+(9, '004', '2011-10-22', '2018-12-31'),
+(9, '001', '2019-01-01', now()),
+(10, '004', '2018-01-19', now()),
+(11, '003', '2021-01-03', '2021-06-30'),
+(11, '002', '2021-07-01', now()),
+(12, '005', '2010-09-30', now()),
+(13, '001', '2011-10-01', '2015-12-31'),
+(13, '003', '2016-01-01', now()),
+(14, '004', '2012-11-29', '2016-11-29'),
+(14, '001', '2016-11-30', now()),
+(15, '005', '2015-06-18', '2019-09-09'),
+(15, '004', '2019-09-10', now());
+
+
+
+/* Insert titles */
+
+INSERT INTO 
+titles (emp_no, title, from_date, to_date) 
+values 
+(1, 'Master in Psychology', '2015-02-13', null),
+(2, 'BA in Business', '2020-09-23', null),
+(3, 'BA in Commerce', '2018-04-01', null),
+(4, 'MA in Law', '2021-01-02', null),
+(5, 'BA in Economics', '2016-09-07', null),
+(6, 'BA in Administration', '2011-08-31', null),
+(7, 'BA in Financial Studies', '2017-09-22', null),
+(8, 'BA in Accounting', '2020-09-01', null),
+(9, 'Master in Linguistics', '2010-10-01', null),
+(10, 'Master in Law', '2020-10-11', null),
+(11, 'Master in Statistics', '2020-12-04', null),
+(12, 'BA in Administration', '2010-09-01', null),
+(13, 'BA in Commerce', '2020-10-04', null),
+(14, 'BA in Economics', '2012-11-02', null),
+(15, 'Master in Economics', '2014-12-22', null);
+
+
+
+
+/* UPDATE DATA */
+
+/* Change the name of an employee. To do this, generate a query that
+affects only a certain employee based on their name, surname and
+date of birth. */
+
+UPDATE employees.employees 
+SET first_name = 'Paul'
+WHERE (
+first_name = 'John' AND
+last_name LIKE 'm%' AND 
+birth_date = '1989-01-22'
+);
+
+
+
+/* Change the name of all departments. */
+
+UPDATE employees.departments
+     SET dept_name =
+     CASE
+     WHEN dept_no = '001' THEN 'HR MANAGEMENT'
+     WHEN dept_no = '002' THEN 'PRODUCTION AND SALES'
+     WHEN dept_no = '003' THEN 'PURCHASING AND TRADING'
+     WHEN dept_no = '004' THEN 'ADVERTISING AND MARKETING'
+     WHEN dept_no = '005' THEN 'ACCOUNTING'
+     END;
+
+
+
+/* Select all employees with a salary greater than 20,000 */
+
   SELECT * 
   FROM employees 
   INNER JOIN salaries 
-  ON employees.emp_no = salaries.emp_no AND 
-    salaries.salary > 20000;
+  ON employees.emp_no = salaries.emp_no AND salaries.salary > 20000;
 
-  -- Select all employees with a salary below 10,000.
-  SELECT * 
-  FROM employees 
-  INNER JOIN salaries 
-  ON employees.emp_no = salaries.emp_no AND 
-    salaries.salary < 10000;
 
-  -- Select all employees who have a salary between 14,000 and 50,000.
-  SELECT * 
-  FROM employees 
-  INNER JOIN salaries 
-  ON employees.emp_no = salaries.emp_no AND 
-    salaries.salary BETWEEN 14000 AND 50000;
 
-  -- Select the total number of employees.
-  SELECT COUNT(emp_no) FROM employees;
+/* Select all employees with a salary below 10,000 */
+  SELECT *
+  FROM employees
+  INNER JOIN salaries
+  ON employees.emp_no = salaries.emp_no AND salaries.salary < 10000;
 
-  -- Select the total number of employees who have worked in more than one department.
-  SELECT COUNT(emp_no) 
-  FROM (
-    SELECT emp_no 
-    FROM dept_emp 
-    GROUP BY emp_no 
-    HAVING COUNT(emp_no) > 1
+
+
+/* Select all employees who have a salary between 14.00 and 50,000 */
+
+  SELECT *
+  FROM employees
+  INNER JOIN salaries
+  ON employees.emp_no = salaries.emp_no AND
+   salaries.salary BETWEEN 14000 AND 50000;
+
+
+
+/* Select the total number of employees */
+
+SELECT COUNT(emp_no) FROM employees;
+
+SELECT COUNT(emp_no) 
+FROM ( 
+  SELECT emp_no 
+  FROM dept_emp 
+  GROUP BY emp_no 
+  HAVING COUNT(emp_no) > 1 
   ) employees;
 
-  -- Select the titles of the year 2019.
+
+
+/* Select the titles of the year 2019 (n my case it is empty set)*/
+SELECT * 
+FROM titles
+WHERE from_date
+BETWEEN '2019-01-01' AND '2019-12-31' 
+
+
+
+/* Select the titles of the year 2020 */
   SELECT * 
-  FROM titles 
-  WHERE from_date 
-    BETWEEN '2019-01-01' AND '2019-12-31';
+FROM titles
+WHERE from_date
+BETWEEN '2020-01-01' AND '2020-12-31' 
 
-  -- Select only the name of the employees in capital letters.
-  SELECT UCASE(first_name) FROM employees;
 
-  -- Select the name, surname and name of the current department of each employee.
-  SELECT E.first_name, E.last_name, D.dept_name
-  FROM employees E 
-  JOIN (
-    SELECT emp_no, dept_no, from_date 
-    FROM dept_emp 
-    WHERE from_date IN (
-      SELECT MAX(from_date) 
-      FROM dept_emp 
-      GROUP BY emp_no
-    )
-  ) recent_depts ON E.emp_no = recent_depts.emp_no 
-  JOIN departments D ON recent_depts.dept_no = D.dept_no 
-  ORDER BY E.emp_no;
 
-  -- Only showing employees who are currently assigned to a department
-  -- SELECT E.first_name, E.last_name, D.dept_name
-  -- FROM employees E 
-  -- JOIN (
-  --   SELECT emp_no, dept_no, to_date 
-  --   FROM dept_emp 
-  --   WHERE to_date IN (
-  --     SELECT to_date 
-  --     FROM dept_emp 
-  --     WHERE to_date >= CURDATE()
-  --     GROUP BY emp_no
-  --   )
-  -- ) recent_depts ON E.emp_no = recent_depts.emp_no 
-  -- JOIN departments D ON recent_depts.dept_no = D.dept_no 
-  -- ORDER BY E.emp_no;
+/* Select only the name of the employees in capital letters */
 
-  -- Select the name, surname and number of times the employee has worked as a manager.
-  SELECT E.first_name, E.last_name, M.times_manager 
-  FROM employees E 
-  LEFT JOIN (
-    SELECT emp_no, COUNT(*) AS times_manager 
-    FROM dept_manager 
+/* 1 */
+SELECT UPPER(first_name) FROM employees;
+
+/* 2 */
+SELECT UCASE(first_name) FROM employees;
+
+
+
+/* Select the name, surname and name of the current department of each employee */
+SELECT employees.first_name, employees.last_name, departments.dept_no, departments.dept_name
+FROM employees
+INNER JOIN (
+  SELECT emp_no, dept_no, from_date
+  FROM dept_emp
+  WHERE from_date IN (
+    SELECT MAX(from_date)
+    FROM dept_emp
     GROUP BY emp_no
-  ) M ON E.emp_no = M.emp_no;
+  )
+) current_dept ON employees.emp_no = current_dept.emp_no
+INNER JOIN departments ON current_dept.dept_no = departments.dept_no;
 
-  -- Select the name without any being repeated.
-  SELECT DISTINCT first_name FROM employees;
 
--- 4. DATA DELETION:
 
-  -- Eliminate all employees with a salary greater than 20,000 (assuming the salary paid today).
-  DELETE FROM employees 
-    WHERE emp_no IN (
-      SELECT E.emp_no 
-      FROM employees E 
-      JOIN salaries S ON E.emp_no = S.emp_no 
-      WHERE S.salary > 20000 AND S.to_date >= CURDATE()
-    );
+/* Select the name, surname and number of times the employee has worked as
+a manager */
 
-  -- Remove the department that has the most employees.
-  DELETE FROM departments 
-  WHERE dept_no = (
-    SELECT dept_no 
-    FROM dept_emp 
-    WHERE to_date >= CURDATE() 
-    GROUP BY dept_no
-    ORDER BY COUNT(DISTINCT emp_no) DESC 
-    LIMIT 1
-  );
-  
-  -- ALTERNATIVE 2 --> ERROR: 'LIMIT & IN/ALL/ANY/SOME subquery' not supported by MariaDB
-  -- DELETE FROM departments 
-  --   WHERE dept_no IN (
-  --     SELECT dept_no, COUNT(DISTINCT emp_no) AS current_emp 
-  --     FROM dept_emp 
-  --     WHERE to_date >= CURDATE() 
-  --     GROUP BY dept_no
-  --     ORDER BY current_emp DESC 
-  --     LIMIT 1
-  --   );
+SELECT CONCAT(first_name, ' ', last_name) AS 'full_name', managers.number_of_time
+FROM employees
+LEFT JOIN (
+SELECT emp_no, COUNT(*) AS number_of_time
+FROM dept_manager 
+GROUP BY emp_no
+) managers on employees.emp_no = managers.emp_no;
+
+
+
+/* Select the name of employees without any being repeated */
+
+SELECT DISTINCT first_name FROM employees;
+
+
+
+/* Eliminate all employees with a salary greater than 20,000 */
+
+/* 1 */
+DELETE employees.*
+FROM employees
+INNER JOIN salaries ON employees.emp_no = salaries.emp_no 
+WHERE salaries.salary > 20000;
+
+/* 2 */
+DELETE 
+FROM employees
+WHERE emp_no IN (
+  SELECT emp_no FROM salaries
+  WHERE salaries.salary > 20000
+  GROUP BY emp_no
+);
+
+
+
+/* Remove the department that has more employees */
+
+DELETE FROM departments
+WHERE dept_no = (
+SELECT dept_no 
+FROM dept_emp   
+GROUP BY dept_no
+ORDER BY COUNT(*) DESC 
+limit 1
+);
+
