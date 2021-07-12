1849c8b37b9154c94816fffc3a0e64a561b842aa
Marc <marcsr03@gmail.com>
2021-07-12 17:18:49 +0200

    finished select queries

--- updateDataQueries.sql
+++ updateDataQueries.sql
@@ -5,6 +5,7 @@ UPDATE employees SET first_name = 'Marc', last_name = 'Sola'
 WHERE first_name = 'Clark' AND last_name = 'Ken' AND birth_date ='1998-12-01';
 
 -- Change the name of all departments in one query.
+-- 1st way
 INSERT INTO departments(dept_no, dept_name)
 VALUES 
 (CHAR(102,105,110), 'finance department'), 
@@ -13,3 +14,8 @@ VALUES
 (CHAR(115,101,114), 'service department'), 
 (CHAR(115,117,112), 'supply department') 
 ON DUPLICATE KEY UPDATE dept_name = VALUES(dept_name); 
+
+-- 2nd way
+UPDATE departments
+SET dept_name = CONCAT(dept_name, ' department');
+
