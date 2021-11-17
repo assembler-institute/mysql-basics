4508e37b5064158c578f9ccdc4474ee9b9dc46c9
Daniella Alamo <dalamov08@gmail.com>
2021-11-17 23:47:22 +0100

    Create solution.sql

---
+++ solution.sql
@@ -0,0 +1,24 @@
+-- insert a employee
+INSERT INTO employees VALUES(1, '2001-08-21', 'Meredith', 'Palmer', 'F', '2021-10-30');
+INSERT INTO employees VALUES(2, '1968-09-07', 'Michael', 'Scott', 'M', '2004-10-30');
+INSERT INTO employees VALUES(3, '1979-05-21', 'Andy', 'Bernard', 'M', '2006-09-08');
+INSERT INTO employees VALUES(4, '1971-01-17', 'Dwight', 'Schrute', 'M', '2004-10-16');
+INSERT INTO employees VALUES(5, '1983-03-12', 'Pam', 'Beesley', 'F', '2006-02-11');
+INSERT INTO employees VALUES(6, '1980-04-11', 'Kelly', 'Kapoor', 'F', '2006-03-10');
+INSERT INTO employees VALUES(7, '1981-02-08', 'Jim', 'Halpert', 'M', '2004-12-11');
+INSERT INTO employees VALUES(8, '1959-10-07', 'Creed', 'Bratton', 'M', '2003-08-30');
+INSERT INTO employees VALUES(9, '1974-11-14', 'Angela', 'Martin', 'F', '2004-11-02');
+INSERT INTO employees VALUES(10, '1979-06-12', 'Ryan', 'Howard', 'M', '2007-02-18');
+INSERT INTO employees VALUES(11, '1977-04-03', 'Toby', 'Flenderson', 'M', '2001-10-31');
+INSERT INTO employees VALUES(12, '1970-01-09', 'Kevin', 'Malone', 'M', '2005-05-30');
+INSERT INTO employees VALUES(13, '1978-11-03', 'Gabe', 'Lewis', 'M', '2009-01-07');
+INSERT INTO employees VALUES(14, '1975-09-27', 'Darryl', 'Philbin', 'M', '2006-09-17');
+INSERT INTO employees VALUES(15, '1969-10-26', 'Phyllis', 'Lapin-Vance', 'F', '2004-05-08');
+INSERT INTO employees VALUES(16, '1975-09-27', 'Kelly', 'Erinhannon', 'M', '2006-09-17');
+INSERT INTO employees VALUES(17, '1969-10-26', 'Michael', 'Thompson', 'F', '2004-05-08');
+INSERT INTO employees VALUES(18, '1969-10-26', 'Jim', 'James', 'F', '2004-05-08');
+
+
+-- salaries
+INSERT INTO salaries VALUES(4,'7000','2007-10-12', '2008-09-01');
+
