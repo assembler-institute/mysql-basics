8986fc55755a125395b6824dd174cafadc2e4278
Antonio Rufino <devs.toni@gmail.com>
2023-01-20 13:05:09 +0100

    last modif delete query

--- queries.sql
+++ queries.sql
@@ -231,7 +231,11 @@ delete from departments
     and dept_no 
     not in (select dept_no from dept_manager);
 
--- department with more employees
+-- department with more employees - 1 employee
+delete from departments 
+  where dept_no = (select s.dept_no from (select COUNT(dept_no) times, dept_no from dept_emp group by dept_no) as s having max(s.times));
+
+-- department with more employees (extra)
 delete from departments 
   where dept_no in 
     (select m.dept_no from (select de.dept_no, COUNT(de.dept_no) times from dept_emp de group by de.dept_no) as m
