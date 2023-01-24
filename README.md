cbbf82ec63249d182c15593c0eb2864683ce030c
Jesus Velazquez <jesusfvelazquez@hotmail.com>
2023-01-24 19:23:00 +0100

    finished

--- README.md
+++ README.md
@@ -13,3 +13,73 @@ This repository contains only the following files:
 - **_README.md_**: this explanatory file.
 - **_employees.sql_**: file containing an example database provided by the official MySQL team, to import and use in our exercise.
 - **_queries.sql_**: file where we recorded all the necessary queries executed to fulfill this exercise.
+- **_backup.sql_**: file with the backup from the employees database as an example of backup.
+
+### IMAGES FROM THE DATABASE CREATED
+
+![Screenshot](database_company.png)
+
+### THINGS TO HAVE IN MIND
+
+To be able to succesfully finalize this project make sure you have a database management system (DMS) based in SQL such as MySQL. For those of you who already have XAMPP in your computer you don't need to download anything else for it already comes with a DMS. In my case it came with MariaDB Server. This is one of the most popular open source relational databases. It’s made by the original developers of MySQL so full compatibility.
+Only, make sure to look for the right syntax only when writing code according to the DMS version you have.
+
+To check whereas you have it already install run this in your terminal (the syntax may vary depending, make sure you find it online it a syntax error occurs):
+
+```
+../bin/mysql --version
+```
+
+This will return the version of the DMS you have. It it returns nothing or an error you will have to download a DMS.
+
+Next we enter the DMS with this code:
+
+```
+../bin/mysql -u root -p
+```
+
+Now we are inside and need to create a username for we have entered as the root user with all the permissions. We want to make sure we don't touch anything we don't want to. For that:
+
+```
+CREATE USER username@localhost IDENTIFIED BY 'password';
+```
+
+In username change to your likely username and in password select your own.
+Next we need to grant privileges on that very own user we created with:
+
+```
+GRANT ALL PRIVILEGES ON * . * TO username@localhost;
+```
+
+Although in my version I needed to use:
+
+```
+GRANT USAGE ON *.* TO 'username'@'localhost' IDENTIFIED BY 'password';
+```
+
+If we exit the DMS with:
+
+```
+exit;
+```
+
+and try now with:
+
+```
+../bin/mysql -u username -p
+```
+
+we should be able to enter the password now, press enter and voilà, we shoul enter our user.
+
+Next you need to download a tool to help you out with the visualization of the database and its tables. I used phpMyAdmin.
+It is a free software tool written in PHP, intended to handle the administration of MySQL over the Web. There are other softwares such as workbench but it doesn't work that well in mac.
+
+And then, work in the database.
+
+Pro-tip: if you would like to create a back up of your database you can run this command in the terminal:
+
+```
+../bin/mysqldump -u username -pPassword databaseName > /path/path/path/fileName.sql;
+```
+
+And a sql file will be created with the code necessary to instaurate your database in case needed.
---
+++ backup.sql
@@ -0,0 +1,257 @@
+-- MariaDB dump 10.19  Distrib 10.4.27-MariaDB, for osx10.10 (x86_64)
+--
+-- Host: localhost    Database: employees
+-- ------------------------------------------------------
+-- Server version	10.4.27-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Temporary table structure for view `current_dept_emp`
+--
+
+DROP TABLE IF EXISTS `current_dept_emp`;
+/*!50001 DROP VIEW IF EXISTS `current_dept_emp`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE VIEW `current_dept_emp` AS SELECT
+ 1 AS `emp_no`,
+  1 AS `dept_no`,
+  1 AS `from_date`,
+  1 AS `to_date` */;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `departments`
+--
+
+DROP TABLE IF EXISTS `departments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `departments` (
+  `dept_no` char(4) NOT NULL,
+  `dept_name` varchar(40) NOT NULL,
+  PRIMARY KEY (`dept_no`),
+  UNIQUE KEY `dept_name` (`dept_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `departments`
+--
+
+LOCK TABLES `departments` WRITE;
+/*!40000 ALTER TABLE `departments` DISABLE KEYS */;
+INSERT INTO `departments` VALUES ('BLUE','Blue'),('BRWN','Brown'),('GRNN','Green'),('ORNG','Orange'),('PRPL','Purple'),('WHTE','White'),('YLLW','Yellow');
+/*!40000 ALTER TABLE `departments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dept_emp`
+--
+
+DROP TABLE IF EXISTS `dept_emp`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dept_emp` (
+  `emp_no` int(11) NOT NULL,
+  `dept_no` char(4) NOT NULL,
+  `from_date` date NOT NULL,
+  `to_date` date NOT NULL,
+  PRIMARY KEY (`emp_no`,`dept_no`),
+  KEY `dept_no` (`dept_no`),
+  CONSTRAINT `dept_emp_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,
+  CONSTRAINT `dept_emp_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dept_emp`
+--
+
+LOCK TABLES `dept_emp` WRITE;
+/*!40000 ALTER TABLE `dept_emp` DISABLE KEYS */;
+INSERT INTO `dept_emp` VALUES (1,'BLUE','2000-07-21','2022-08-10'),(1,'YLLW','2022-11-01','2023-01-20'),(2,'BLUE','2019-05-14','2023-01-20'),(2,'GRNN','1998-03-10','2019-05-13'),(3,'BRWN','2010-02-05','2020-08-09'),(4,'BLUE','2007-09-11','2013-04-08'),(4,'YLLW','2013-04-09','2023-01-20'),(12,'ORNG','2008-02-01','2023-01-20'),(14,'BLUE','2022-09-09','2023-01-20'),(14,'YLLW','2020-01-01','2022-09-08'),(15,'GRNN','2018-02-21','2019-05-13');
+/*!40000 ALTER TABLE `dept_emp` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary table structure for view `dept_emp_latest_date`
+--
+
+DROP TABLE IF EXISTS `dept_emp_latest_date`;
+/*!50001 DROP VIEW IF EXISTS `dept_emp_latest_date`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE VIEW `dept_emp_latest_date` AS SELECT
+ 1 AS `emp_no`,
+  1 AS `from_date`,
+  1 AS `to_date` */;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `dept_manager`
+--
+
+DROP TABLE IF EXISTS `dept_manager`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dept_manager` (
+  `emp_no` int(11) NOT NULL,
+  `dept_no` char(4) NOT NULL,
+  `from_date` date NOT NULL,
+  `to_date` date NOT NULL,
+  PRIMARY KEY (`emp_no`,`dept_no`),
+  KEY `dept_no` (`dept_no`),
+  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,
+  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dept_manager`
+--
+
+LOCK TABLES `dept_manager` WRITE;
+/*!40000 ALTER TABLE `dept_manager` DISABLE KEYS */;
+INSERT INTO `dept_manager` VALUES (1,'YLLW','2022-11-01','2023-01-20');
+/*!40000 ALTER TABLE `dept_manager` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employees`
+--
+
+DROP TABLE IF EXISTS `employees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employees` (
+  `emp_no` int(11) NOT NULL AUTO_INCREMENT,
+  `birth_date` date NOT NULL,
+  `first_name` varchar(14) NOT NULL,
+  `last_name` varchar(16) NOT NULL,
+  `gender` enum('M','F') NOT NULL,
+  `hire_date` date NOT NULL,
+  PRIMARY KEY (`emp_no`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employees`
+--
+
+LOCK TABLES `employees` WRITE;
+/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
+INSERT INTO `employees` VALUES (1,'1975-03-10','Eduardo','Gomez Rubico','M','2000-07-21'),(2,'1968-08-01','Eduardo','Vazquez Froslan','M','1998-03-10'),(3,'1982-10-19','Miquel','Crack Geostatic','M','2010-02-05'),(4,'1985-12-25','Ana','Fernandez Perez','F','2007-09-11'),(12,'1970-05-08','Fuentes','Agua Clara','F','2008-02-01'),(14,'1972-10-03','Chusa','Sernoida Thomas','F','2020-01-01'),(15,'1987-04-18','Amparo','Noche Dia','F','2018-02-21');
+/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `salaries`
+--
+
+DROP TABLE IF EXISTS `salaries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `salaries` (
+  `emp_no` int(11) NOT NULL,
+  `salary` int(11) NOT NULL,
+  `from_date` date NOT NULL,
+  `to_date` date NOT NULL,
+  PRIMARY KEY (`emp_no`,`from_date`),
+  CONSTRAINT `salaries_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `salaries`
+--
+
+LOCK TABLES `salaries` WRITE;
+/*!40000 ALTER TABLE `salaries` DISABLE KEYS */;
+INSERT INTO `salaries` VALUES (1,6500,'2002-05-12','2022-08-10'),(1,10600,'2022-11-01','2023-01-20'),(2,12800,'1998-03-10','2019-05-13'),(2,18500,'2019-05-14','2023-01-20'),(3,19800,'2010-02-05','2020-08-09'),(4,13750,'2007-09-11','2013-04-08'),(4,20000,'2013-04-09','2023-01-20'),(12,15250,'2008-02-01','2023-01-20'),(14,10300,'2020-01-01','2022-09-08'),(14,20000,'2022-09-09','2023-01-20'),(15,17200,'2018-02-21','2019-05-13');
+/*!40000 ALTER TABLE `salaries` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `titles`
+--
+
+DROP TABLE IF EXISTS `titles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `titles` (
+  `emp_no` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `from_date` date NOT NULL,
+  `to_date` date DEFAULT NULL,
+  PRIMARY KEY (`emp_no`,`title`,`from_date`),
+  CONSTRAINT `titles_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `titles`
+--
+
+LOCK TABLES `titles` WRITE;
+/*!40000 ALTER TABLE `titles` DISABLE KEYS */;
+INSERT INTO `titles` VALUES (1,'Marketing','1995-01-01','2000-01-01'),(2,'Bussiness Administration','1992-01-01','1997-01-01'),(3,'Computer engineering','2015-01-01','2020-01-01'),(4,'Accounting and finances','1999-01-01','2003-01-01'),(12,'Events and Protocol','2015-01-01','2020-01-01'),(14,'Marketing','2016-01-01','2020-01-01'),(15,'Accounting and finances','1994-01-01','1999-01-01');
+/*!40000 ALTER TABLE `titles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Final view structure for view `current_dept_emp`
+--
+
+/*!50001 DROP VIEW IF EXISTS `current_dept_emp`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_unicode_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`jesus`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `current_dept_emp` AS select `l`.`emp_no` AS `emp_no`,`d`.`dept_no` AS `dept_no`,`l`.`from_date` AS `from_date`,`l`.`to_date` AS `to_date` from (`dept_emp` `d` join `dept_emp_latest_date` `l` on(`d`.`emp_no` = `l`.`emp_no` and `d`.`from_date` = `l`.`from_date` and `l`.`to_date` = `d`.`to_date`)) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `dept_emp_latest_date`
+--
+
+/*!50001 DROP VIEW IF EXISTS `dept_emp_latest_date`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_unicode_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`jesus`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `dept_emp_latest_date` AS select `dept_emp`.`emp_no` AS `emp_no`,max(`dept_emp`.`from_date`) AS `from_date`,max(`dept_emp`.`to_date`) AS `to_date` from `dept_emp` group by `dept_emp`.`emp_no` */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-01-24 11:12:29
